fc58625a5b3e0f3d0ec8fb13097a7430422eb2a6
Teodor <trteodor@gmail.com>
2022-05-19 19:42:40 +0200

    Can't compile

---
+++ CM7/Core/Inc/DispTest.h
@@ -0,0 +1,15 @@
+/*
+ * DispTest.h
+ *
+ *  Created on: May 19, 2022
+ *      Author: teodor
+ */
+
+#ifndef INC_DISPTEST_H_
+#define INC_DISPTEST_H_
+
+void LCD_Task(void);
+
+void LCD_OTM8009a_InitFull(void);
+
+#endif /* INC_DISPTEST_H_ */
